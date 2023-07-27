8f97a1d6fae8559203af9adaa2ec0c612f0c25a8
Jayson Thomas <jayson.j.thomas@gmail.com>
2023-07-27 15:49:13 +0100

    Some clean up required.

---
+++ pyexamples/estimation/leastSquares.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy.linalg as LA
+
+def generateH(m, n):
+  """
+  Creates a matrix where each row is of the form: 
+  [t**degree, t**(degree-1), ..., 1] 
+  """
+  t = np.random.uniform(-5, 5, m)
+  H = np.zeros((m, n))
+  for i in np.arange(n-1, -1, -1):
+    H[:, -i-1] = t**i
+    # print(f'a{-i-1}, {t**i}')
+  return H, t
+
+def collectMeasurements(m, n):
+  H, t = generateH(m, n)
+  # Create m noisy observations, where the noise distribution is gaussian
+  yTilde = H @ x + np.random.normal(0, 1, size=(m))
+  print(f'yTilde {yTilde.shape}: {yTilde}')
+  plt.plot(t, yTilde, 'bx')
+  plt.title("Noisy Observations")
+  plt.show()
+
+  return yTilde, H
+
+# Setup
+# m: number of samples/measurements - the larger the value the more accurate x hat will be.
+# n: size of state
+# x: state, unknown in practice
+m = 100
+n = 4
+x = np.random.randn(n) * 2 # np.array([8, 4, 2, 1])
+
+yTilde, H = collectMeasurements(m, n)
+print(f'H: {H.shape}')
+xHat = LA.pinv(H.T @ H) @ H.T @ yTilde
+
+print(xHat)
+print(x)
+
