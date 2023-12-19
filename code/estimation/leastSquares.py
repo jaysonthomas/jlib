47080626f1b706349d021c315190f4594eb1941e
Jayson Thomas <jayson.j.thomas@gmail.com>
2023-12-19 15:59:12 +0000

    Finished 1st proper note on batch LS corresponding to jlib.

--- code/estimation/leastSquares.py
+++ code/estimation/leastSquares.py
@@ -11,31 +11,26 @@ def generateH(m, n):
   H = np.zeros((m, n))
   for i in np.arange(n-1, -1, -1):
     H[:, -i-1] = t**i
-    # print(f'a{-i-1}, {t**i}')
   return H, t
 
 def collectMeasurements(x, m, n):
   H, t = generateH(m, n)
   # Create m noisy observations, where the noise distribution is gaussian
-  yTilde = H @ x + np.random.normal(0, 1, size=(m))
-  print(f'yTilde {yTilde.shape}: {yTilde}')
-  plt.plot(t, yTilde, 'bx')
+  y = H @ x + np.random.normal(0, 1, size=(m))
+  plt.plot(t, y, 'bx')
   plt.title("Noisy Observations")
   plt.show()
 
-  return yTilde, H
+  return y, H
 
 # Setup
-# m: number of samples/measurements - the larger the value the more accurate x hat will be.
-# n: size of state
-# x: state, unknown in practice
 m = 100
 n = 4
 x = np.random.randn(n) * 2 # np.array([8, 4, 2, 1])
 
-yTilde, H = collectMeasurements(m, n)
+y, H = collectMeasurements(x, m, n)
 print(f'H: {H.shape}')
-xHat = LA.pinv(H.T @ H) @ H.T @ yTilde
+xHat = LA.pinv(H.T @ H) @ H.T @ y
 
 print(xHat)
 print(x)
